c1410b436a9b9c69fa420d95d3adc8c299aacf3f
Firas-amaria <128461535+Firas-amaria@users.noreply.github.com>
2025-04-06 14:53:34 +0300

    main menu fin
    added main menu scene + level-01 scene
    level-01 scene is empty atm
    main menu buttons work as they should

---
+++ Assets/scripts/main_menu_script.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class main_menu_script : MonoBehaviour
+{
+    public GameObject settings_panel;
+    public void start_game()
+    {
+        SceneManager.LoadScene("level-01");
+    }
+
+    public void exit_game()
+    {
+        Debug.Log("Exiting game");
+        Application.Quit();
+    }
+
+    public void open_settings()
+    {
+        settings_panel.SetActive(true);
+    }
+
+    public void close_settings()
+    {
+        settings_panel.SetActive(false);
+    }
+
+
+}
